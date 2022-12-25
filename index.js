986f0c3d6dc3c41fb29588ccd7029bf832ba27b3
Skopfy <christian.roth.1995.de@gmail.com>
2022-12-25 22:27:55 +0100

    3.3 works

---
+++ index.js
@@ -0,0 +1,52 @@
+const http = require('http')
+
+const express = require('express')
+const app = express()
+
+let data = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    },
+]
+
+
+app.get('/api/persons', (request, response) => {
+    response.json(data)
+})
+
+app.get('/api/info', (request, response) => {
+    const len = data.length
+    const date = new Date()
+    response.send(`The phonebook site has ${len} people.`+ `\n` + date)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = data.find(person => person.id === id)
+    if (person) {
+	response.json(person)
+    } else {
+	response.status(404).end()
+    }
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
