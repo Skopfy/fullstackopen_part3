0c146acb5e7006a70808d4b2c6677e6150907b92
Skopfy <christian.roth.1995.de@gmail.com>
2022-12-25 23:07:56 +0100

    3.6 works

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http')
 
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 let data = [
     { 
@@ -26,6 +27,12 @@ let data = [
     },
 ]
 
+const generateId = () => {
+  const randId = data.length > 0
+	? Math.floor(Math.random() * 100000)
+    : 0
+  return randId
+}
 
 app.get('/api/persons', (request, response) => {
     response.json(data)
@@ -45,6 +52,43 @@ app.get('/api/persons/:id', (request, response) => {
     } else {
 	response.status(404).end()
     }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  data = data.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  } else if (!body.number) {
+      return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+
+    const existing = data.find(person => person.name === body.name)
+    if (existing) {
+    return response.status(400).json({ 
+	error: 'name duplicate, exists already' 
+    })
+  }
+
+    const person = {
+	id: generateId(),
+	name: body.name,
+	number: body.number,
+  }
+
+  data = data.concat(person)
+
+  response.json(person)
 })
 
 const PORT = 3001
