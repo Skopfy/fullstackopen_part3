0323aa6228204d350ca1b1aeee934e7fbbb2e6d4
Skopfy <christian.roth.1995.de@gmail.com>
2022-12-27 21:49:27 +0100

    Added CORS

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 app.use(express.json())
 morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
-//app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+app.use(cors())
+
 let data = [
     { 
       "id": 1,
