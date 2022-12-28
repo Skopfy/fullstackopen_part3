2b04a2e965649ca7e2e4f74abce46df49aec544e
Skopfy <christian.roth.1995.de@gmail.com>
2022-12-28 13:21:11 +0100

    3b works

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 app.use(express.json())
+app.use(express.static('build'))
 morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 app.use(cors())
@@ -96,6 +97,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
