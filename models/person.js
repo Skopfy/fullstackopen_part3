6203787c0dd68c6a0a9c7ece00a228fcd00e1f2d
Skopfy <christian.roth.1995.de@gmail.com>
2023-01-02 15:02:36 +0100

    3.20 done except HTTP Post duplicates

--- models/person.js
+++ models/person.js
@@ -5,9 +5,31 @@ console.log('connecting to', url)
 mongoose.connect(url)
     .then(result => {    console.log('connected to MongoDB')  })  .catch((error) => {    console.log('error connecting to MongoDB:', error.message)  })
 
+function number_parts_validator (val) {
+    var validation = true
+    const parts = val.split("-")
+    if ((parts.length !== 2) || (parts[0].length !== 2 && parts[0].length !== 3) || (parts[1].length === 0)) {
+	validation = false
+    }
+    if (!(/^\d+$/.test(parts[0]) || !(/^\d+$/.test(parts[1])))) { //Check if its a number, i.e. only digits
+	validation = false
+    }
+    return validation;
+}
+
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+    name: {
+	type: String,
+	minLength: 3
+    },
+    number: {
+	type: String,
+	minLength: 8,
+	validate: {
+	    validator: number_parts_validator,
+	    msg: 'Phone number must have schema P0-P1 where P0 is two or three digits long and P1 of at least one digit.'
+    },
+    },
 })
 
 personSchema.set('toJSON', {
